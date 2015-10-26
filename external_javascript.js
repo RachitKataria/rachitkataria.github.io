0324438d1fef7cd8dd3cbd62fd8bfa31914fa56d
Rachit Kataria <rachitka@usc.edu>
2015-10-26 01:54:39 -0700

    Modified sizing of background JS animation

--- external_javascript.js
+++ external_javascript.js
@@ -81,8 +81,8 @@ particlesJS("particles-js", {
       },
       "image": {
         "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+        "width": 50,
+        "height": 50
       }
     },
     "opacity": {
